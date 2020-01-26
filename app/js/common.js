cf64838104ba9ce268736063395154e2cd1ad8f5
Vasilii <goooneoooogle@gmail.com>
2020-01-26 18:43:46 +0200

    Finish main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,51 @@
 $(function() {
-
+  $(".reviews--container").slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: $('.reviews--slider__prev'),
+    prevArrow: $('.reviews--slider__next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+  $(".portfolio--slider").slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: $('.portfolio--slider__prev'),
+    prevArrow: $('.portfolio--slider__next'),
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 });
